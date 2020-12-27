dbfa0d623e40caa347d3e84a6047229f43afb730
Yordan Mladenov <yordanmladenov@gmail.com>
2020-12-27 21:29:25 +0200

    Add enable_if examples Add impl of std::enable_if

---
+++ enable_if/src/enable_if.cpp
@@ -0,0 +1,69 @@
+//============================================================================
+// Name        : enable_if.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Section 28.4 enable_if
+//============================================================================
+
+#include <iostream>
+#include <type_traits>
+#include <iterator>
+
+template<bool B, typename T>
+using Enable_if = typename std::enable_if<B,T>::type;
+
+template<typename T>
+constexpr bool Is_class()
+{
+    return std::is_class<T>::value;
+}
+
+template<typename T>
+struct Smart_pointer {
+    // ...
+    T& operator*() { return *p; }                             //return reference to whole object
+    template<class T1=T>
+    Enable_if<Is_class<T1>(),T1>* operator->() { return p; }  // select a member (for classes only)
+    // ...
+
+private:
+    T* p;
+};
+
+template<typename Scale>
+struct complex {
+    Scale real() { return 1.0; }
+};
+
+template<class I>
+constexpr bool Input_iterator() { return false; }
+
+template<typename T>
+class myvector {
+public:
+    myvector(size_t n, const T& val) {
+        std:: cout << __PRETTY_FUNCTION__;
+    } // n elements of type T with value val
+
+    template<typename Iter, typename =Enable_if<Input_iterator<Iter>(),Iter>>
+            myvector(Iter b, Iter e) {
+        std:: cout << __PRETTY_FUNCTION__;
+    } // initialize from [b:e)
+    // ...
+};
+
+void f(Smart_pointer<double> p, Smart_pointer<complex<double>> q)
+{
+    auto d0 = *p; //OK
+    auto c0 = *q; //OK
+    auto d1 = q->real(); // OK
+    //auto d2 = p->real(); // error : p doesn’t point to a class object
+    // ...
+}
+
+int main()
+{
+    myvector<int> i(10, 20);
+}
+
---
+++ std_enable_if/src/std_enable_if.cpp
@@ -0,0 +1,85 @@
+//============================================================================
+// Name        : std_enable_if.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <type_traits>
+
+///////////////////////////////
+// enable_if implementation
+//////////////////////////////
+template<bool B, typename T = void>
+struct enable_if {
+    typedef T type;
+};
+
+template<typename T>
+struct enable_if<false , T> {};             // no ::type if B==false
+
+template<bool B, typename T = void>
+using Enable_if = typename enable_if<B,T>::type;
+
+///////////////////////////////
+//Enable_if examples (SFINAE)
+//////////////////////////////
+struct failure { };                         // represent a failure to declare something
+
+template<typename T>
+struct succeeded : std::true_type { };
+
+template<>
+struct succeeded<failure> : std::false_type { };
+
+struct Y {};
+
+void f(int i) { }
+void f(const Y& y) { }
+
+template<typename T>
+struct get_f_result {
+private:
+    template<typename X>
+    static auto check(X const& x) -> decltype(f(x));    // can call f(x)
+
+    static failure check(...);                          // cannot call f(x)
+public:
+    using type = decltype(check(std::declval<T>()));
+};
+
+template<typename T>
+struct has_f
+: succeeded<typename get_f_result<T>::type> { };
+
+template<typename T>
+constexpr bool Has_f()
+{
+    return has_f<T>::value;
+}
+
+template<typename T>
+class X {
+public:
+    template<typename T1=T>
+    typename std::enable_if<Has_f<T1>()>::type use_f(const T1& t)
+    {
+        //
+        f(t);
+        //
+    }
+};
+
+int main() {
+    X<int> x;
+    int i{10};
+    x.use_f<int>(i);
+
+    X<Y> x1{};
+    Y y{};
+    x1.use_f(y);
+
+	return 0;
+}
