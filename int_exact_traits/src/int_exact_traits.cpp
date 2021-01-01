52eb5a9890135943cfe22dda54582eedc52ad39f
Yordan Mladenov <yordanmladenov@gmail.com>
2021-01-01 15:34:17 +0200

    Add example with template aliases Section 23.6

---
+++ int_exact_traits/src/int_exact_traits.cpp
@@ -0,0 +1,35 @@
+//============================================================================
+// Name        : int_exact_traits.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Section 23.6 Template aliases
+//============================================================================
+
+#include <iostream>
+
+template<int>
+struct int_exact_traits {                               // idea: int_exact_traits<N>::type is a type with exactly N bits
+    using type = int;
+};
+
+template<>
+struct int_exact_traits<8> {
+    using type = char;
+};
+
+template<>
+struct int_exact_traits<16> {
+    using type = short;
+};
+
+template<int N>
+using int_exact = typename int_exact_traits<N>::type;   // define alias for convenient notation
+
+
+int_exact<8> a = 7; // int_exact<8> is an int with 8 bits
+
+int main() {
+
+	return 0;
+}
